807666eb75b30b2bad419a82d0c0735ec97a530d
mazsu64D <miloszernine.adami@sainsburys.co.uk>
2026-05-05 15:29:53 +0000

    Update Django API root URLs to use CODESPACE_NAME and allow codespace host

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +30,7 @@ DEBUG = True
 
 
 # Allow all hosts
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', os.getenv('CODESPACE_NAME', '')]
 
 
 # Application definition
